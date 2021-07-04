12bbe6bf3c5bec1fe69461adc70166ee3f6002ad
seungrihan <hansr321@gmail.com>
2021-07-04 15:30:30 +0900

    insertionsort

---
+++ insertionsort.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#define MAX 7
+
+void result_print(int n, int arr[]);
+void insertion_sort(int n, int arr[]);
+void swap(int* a, int* b);
+
+int main()
+{
+	int arr[MAX] = {7,5,2,1,4,6,3};
+
+	printf("before array : ");
+	result_print(MAX, arr);
+	insertion_sort(MAX, arr);
+	printf("sorted array : ");
+	result_print(MAX, arr);
+
+	return 0;
+
+}
+
+void result_print(int a, int arr[])
+{
+	for (int a=0; a < MAX; a++)
+	{
+		printf("%d ", arr[a]);
+	}
+	printf("\n");
+}
+
+void insertion_sort(int n, int arr[])
+{
+	for (int i=0; i < n; i++)
+	{
+		int j = i;
+		while (j > 0 && arr[j] < arr[j-1])
+		{
+			swap(&arr[j], &arr[j-1]);
+			j--;
+		}
+		// result_print(MAX, arr); // iteration print
+	}
+}
+
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
